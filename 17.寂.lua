9bedb8ea53f9c10ad7b0a6be583502eb70c77a5a
Keye3Tuido <lonely.stars@qq.com>
2025-12-06 09:13:55 +0800

    code17:免疫地图诅咒

--- 17.寂.lua
+++ 17.寂.lua
@@ -6,4 +6,7 @@
 ---- 代码效果(不用管中文，全选复制即可) ----
 --1.所有房间自动清理，并完全陷入黑暗。无法记忆地图。
 l Isaac.AddCallback({},ModCallbacks.MC_POST_NEW_ROOM,function()local R,T,l,m,t,s,r=RoomDescriptor,RoomType.ROOM_BOSS,Game():GetLevel(),Game():GetRoom(),{}s=l:GetRooms()for i=-18,-1 do t[#t+1]=i end for i=1,#s do t[#t+1]=s:Get(i-1).SafeGridIndex end for _,i in pairs(t)do r=l:GetRoomByIdx(i)r.DisplayFlags,r.Flags=RoomDescriptor.DISPLAY_NONE,r.Flags|R.FLAG_CLEAR|R.FLAG_CHALLENGE_DONE|R.FLAG_PITCH_BLACK|R.FLAG_CURSED_MIST|(r.Data and r.Data.Type~=T and R.FLAG_NO_REWARD or 0)end if m:IsFirstVisit()and T==m:GetType()then m:TriggerClear()end end)
+
+--2. 免疫失忆症、免疫迷途诅咒。
+l local F=Isaac.AddCallback F({},10,function()Game():GetLevel():RemoveCurses(4)end,25)F({},12,function(_,c)return ~4&c end)
 --.
