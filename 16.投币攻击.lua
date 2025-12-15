43f98660bb38484c035f5d6a93dc6448ded5153d
Keye3Tuido <lonely.stars@qq.com>
2025-12-15 11:17:59 +0800

    自动清理代码效果

--- 16.投币攻击.lua
+++ 16.投币攻击.lua
@@ -4,9 +4,10 @@
 
 ---- 代码效果(不用管中文，全选复制即可) ----
 
---0. 删除匿名模组的回调。
+--0. 删除匿名模组的回调。游戏胜利后自动清理代码效果。
 --控制台输入 lua CLM() 可删除所有匿名模组的回调，用于预防重复输入代码和清理代码效果。
-l function CLM()local I,M,t,m=Isaac,'Mod'for i,j in pairs(ModCallbacks)do t=I.GetCallbacks(j)for x=#t,1,-1 do m=t[x][M]if not(m and m.Name)then I.RemoveCallback(m,j,t[x].Function)end end end end CLM()
+--重复输入此代码不额外生效。
+l local A,I,M,t,m=ModCallbacks,Isaac,'Mod'function CLM()for i,j in pairs(A)do t=I.GetCallbacks(j)for x=#t,1,-1 do m=t[x][M]if not(m and m.Name)then I.RemoveCallback(m,j,t[x].Function)end end end end CLM()I.AddCallback({},A.MC_POST_GAME_END,function(_,f)if not f then CLM()end end)
 
 --1. 强制敌人变为精英怪(不包括6无敌变种和25彩虹变种)。
 l local A,I,C={[0]=1,[1]=1,[2]=1,[3]=1,[4]=1,[5]=1,[6]=0,[7]=1,[8]=1,[9]=1,[10]=1,[11]=1,[12]=1,[13]=1,[14]=1,[15]=1,[16]=1,[17]=1,[18]=1,[19]=1,[20]=1,[21]=1,[22]=1,[23]=1,[24]=1,[25]=0},'InitSeed',{}for k,v in pairs(A)do for _=1,v do C[#C+1]=k end end Isaac.AddCallback({}, ModCallbacks.MC_NPC_UPDATE, function(_,e)if e:IsVulnerableEnemy()and e:IsActiveEnemy(false)and not e:IsBoss()and not e:IsInvincible() and not e:IsChampion()then e:MakeChampion(e[I],C[e[I]%#C+1])end end)
--- 6.洒洒水啦~.lua
+++ 6.洒洒水啦~.lua
@@ -5,9 +5,10 @@
 
 ---- 代码效果(不用管中文，全选复制即可) ----
 
---0. 删除匿名模组的回调。
+--0. 删除匿名模组的回调。游戏胜利后自动清理代码效果。
 --控制台输入 lua CLM() 可删除所有匿名模组的回调，用于预防重复输入代码和清理代码效果。
-l function CLM()local I,M,t,m=Isaac,'Mod'for i,j in pairs(ModCallbacks)do t=I.GetCallbacks(j)for x=#t,1,-1 do m=t[x][M]if not(m and m.Name)then I.RemoveCallback(m,j,t[x].Function)end end end end CLM()
+--重复输入此代码不额外生效。
+l local A,I,M,t,m=ModCallbacks,Isaac,'Mod'function CLM()for i,j in pairs(A)do t=I.GetCallbacks(j)for x=#t,1,-1 do m=t[x][M]if not(m and m.Name)then I.RemoveCallback(m,j,t[x].Function)end end end end CLM()I.AddCallback({},A.MC_POST_GAME_END,function(_,f)if not f then CLM()end end)
 
 --1. 所有玩家永久蒙眼（在矿洞逃亡中不生效）。
 l Isaac.AddCallback({},31,function(s,p,g,c,f)f,s,g=1,'Challenge',Game()c=g[s]if p:HasCurseMistEffect()then g[s],f=0 p:TryRemoveNullCostume(14)elseif p:CanShoot()then g[s],f=6 p:AddNullCostume(14)end if not f then p:UpdateCanShoot()end g[s]=c end)
