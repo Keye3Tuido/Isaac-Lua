e9aec5078a13e55d72718c96419ccc6befe2f7f0
Keye3Tuido <Keye3Tuido@example.com>
2026-05-18 20:19:36 +0800

    眼泪替换代码

--- 0.实用代码合辑.lua
+++ 0.实用代码合辑.lua
@@ -120,4 +120,7 @@ l BrokenKeys=3;WaitFrames=90;local A,C,D,M,N,T=Isaac.AddCallback,0,'GetFrameCoun
 
 --38. 将玩家的输入延迟15帧（约0.5秒），可在控制台输入l Lag = 数值 来调整延迟帧数。
 l Lag = 15;local B,C,H,I,M,N,O,T,A,G=table,'ControllerIndex',InputHook,Isaac,ModCallbacks,Input,{},{}A,G=I.AddCallback,I.GetFrameCount A(T,M.MC_POST_PLAYER_RENDER,function(_,p)local t={i=p[C],t=G(),o={}}for k,v in pairs(ButtonAction)do t.o[v]={a=N.IsActionTriggered(v,t.i),p=N.IsActionPressed(v,t.i),v=N.GetActionValue(v,t.i)}end B.insert(O,t)end)A(T,M.MC_INPUT_ACTION,function(_,e,h,a)e=e and e:ToPlayer()local t,r,v=G()for k=#O,1,-1 do v=O[k]r=t-v.t-Lag if r>0 then B.remove(O,k)elseif e and v.i==e[C]and r==0 then if h==H.GET_ACTION_VALUE then return v.o[a].v elseif h==H.IS_ACTION_PRESSED then return v.o[a].p elseif h==H.IS_ACTION_TRIGGERED then return v.o[a].a end end end end)
+
+--39. 将眼泪替换为混沌卡(CHAOS_CARD)/还可替换为橡皮擦(ERAZER)。
+l Isaac.AddCallback({},ModCallbacks.MC_POST_TEAR_INIT,function(_,t)t:ChangeVariant(TearVariant.CHAOS_CARD)end)
 --.
