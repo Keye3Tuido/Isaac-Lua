79d9fad96d037dbab12aa3985e189c232e1f0f10
Keye3Tuido <soarcx@foxmail.com>
2026-08-26 02:16:57 +0800

    限定伯大尼

--- lua/36.魂火盛宴.lua
+++ lua/36.魂火盛宴.lua
@@ -1,5 +1,5 @@
 --魂火盛宴
-
+--限定角色：伯大尼
 
 
 ---- 代码效果 ----
@@ -21,8 +21,8 @@ l local I,G=Isaac,Game()I.AddCallback({},15,function(p,c,t,n)if not c then for _
 -- 格式：c=道具,t/T=饰品(仅保证层数一致),物品={'类别',数量};数量为一时可简化为物品='类别'; 道具id必须>0
 l ITEMS={'c223','c640'}local C,D,E,F,H,I,P,Q,L,M,T=CollectibleType,'OLLECTIBLE','GetPlayerType',PlayerType,'Get',Isaac,pairs,EntityType,'Remove',ModCallbacks,{}local A,B,G,J,K,N,O,S,U=I.AddCallback D,B='C'..D,'C'..D:lower()J=H..B K=J..'Num'O=L..B N=function(i,a,b)a,i=table.unpack(type(i)=='table'and i or{i,1})a,b=a:match('(%a)(%d+)')return i,a,tonumber(b)end G=function(_,a,b,p)for _,i in P(ITEMS)do i,a,b=N(i)if a=='c'then while 0<p[K](p,b)do p[O](p,b)end end end end A(T,M.MC_POST_PLAYER_UPDATE,function(a,p,b,c,e,g,h,j,k,l)if F.PLAYER_THESOUL_B~=p[E](p)and not p:HasCurseMistEffect()then c=I.GetItemConfig()e='Trinket'h='Add'for _,i in P(ITEMS)do i,a,b=N(i)if a=='T'then a='t'i=i*2 end if a=='c'then g=Game():GetItemPool()g[O](g,b)g=0 if not p:IsItemQueueEmpty()then g=p.QueuedItem.Item g=g and g['Is'..B](g)and b==g.ID and 1 or 0 end while i>g+p[K](p,b)do if c[J](c,b).Type==ItemType.ITEM_ACTIVE then S=b break else p[h..B](p,b)end end elseif a=='t'then g=H..e l=h..e while 1 do j=i-p[g..'Multiplier'](p,b)if j<=0 then break end k={}for s=0,1 do k[s]=p[g](p,s)p['Try'..L..e](p,k[s])p[l](p,b|((j>1 and s==0 or j>3)and TrinketType.TRINKET_GOLDEN_FLAG or 0))end p:UseActiveItem(C[D..'_SMELTER'],2315)for s=0,1 do p[l](p,k[s],false)end end end end end end)A(T,M.MC_POST_UPDATE,function(d,r,v,s)if S then r=Game():GetRoom()for _,i in P(DoorSlot)do d=r:GetDoor(i)if d then d:Close()end end d=Q.ENTITY_PICKUP v=PickupVariant.PICKUP_COLLECTIBLE s=I.FindByType if 1>#s(d,v,S)then U=U and U+1 or 1 if U>29 then I.Spawn(d,v,S,r:GetCenterPos(),Vector.Zero,nil)end else U=nil end for _,e in P(s(d,v,0))do e:Remove()end end S=nil end)A(T,M.MC_PRE_USE_ITEM,G,C[D..'_D4'])A(T,M.MC_ENTITY_TAKE_DMG,function(d,e,u,f)d=DamageFlag u='DAMAGE_'e=e:ToPlayer()if F.PLAYER_EDEN_B==e[E](e)and 0==f&(d[u..'RED_HEARTS']|d[u..'IV_BAG']|d[u..'FAKE']|d[u..'NO_PENALTIES'])then G(e,e,e,e)end end,Q.ENTITY_PLAYER)
 
---4. 每秒为灵魂之瓮充能。
-l Isaac.AddCallback({},ModCallbacks.MC_POST_UPDATE,function(a,b,c)b=Game()c=Isaac if b:GetFrameCount()%30<1 then for i=1,b:GetNumPlayers()do a=Vector.Zero a=c.Spawn(EntityType.ENTITY_EFFECT,EffectVariant.ENEMY_SOUL,0,a,a,nil)a.Target=c.GetPlayer(i)a:Remove()end end end)
+--4. 在未清理的房间，每5秒为灵魂之瓮充能。
+l Isaac.AddCallback({},ModCallbacks.MC_POST_UPDATE,function(a,b,c)b=Game()c=Isaac if not    b:GetRoom():IsClear()and b:GetFrameCount()%150<1 then for i=1,b:GetNumPlayers()do a=Vector.Zero a=c.Spawn(EntityType.ENTITY_EFFECT,EffectVariant.ENEMY_SOUL,0,a,a,nil)a.Target=c.GetPlayer(i)a:Remove()end end end)
 
 --5. 将灵魂之瓮的魂火替换为炼狱恶鬼。
 l Isaac.AddCallback({},ModCallbacks.MC_POST_TEAR_INIT,function(_,t)Isaac.Spawn(EntityType.ENTITY_EFFECT,EffectVariant.PURGATORY,1,t.Position,t.Velocity,t.SpawnerEntity):GetSprite():Play('Charge')t:Remove()end,TearVariant.FIRE)
@@ -30,6 +30,9 @@ l Isaac.AddCallback({},ModCallbacks.MC_POST_TEAR_INIT,function(_,t)Isaac.Spawn(E
 --6. 在炼狱恶鬼被移除的位置，触发爆炸效果。
 l Isaac.AddCallback({},ModCallbacks.MC_POST_ENTITY_REMOVE,function(p,e)p=e.Variant==EffectVariant.PURGATORY and e.SubType==1 and e.SpawnerEntity p=p and p:ToPlayer()if p then e=p:FireTear(e.Position,e.Velocity,false,true):ToTear()e:AddTearFlags(TearFlags.TEAR_EXPLOSIVE)e:Die()end end,EntityType.ENTITY_EFFECT)
 
---重开一局新游戏。
-l local A,B,C,Z=Isaac,ModCallbacks.MC_POST_UPDATE,{}Z=function()A.ExecuteCommand'restart'A.RemoveCallback(C,B,Z)end A.AddCallback(C,B,Z)
+--7. 强制角色为游伯大尼。
+l Isaac.AddCallback({},ModCallbacks.MC_POST_PLAYER_UPDATE,function(_,p)local t=PlayerType.PLAYER_BETHANY if t~=p:GetPlayerType()then p:ChangePlayerType(t)end end)
+
+--以伯大尼重开一局新游戏。
+l local A,B,C,Z=Isaac,ModCallbacks.MC_POST_UPDATE,{}Z=function()A.ExecuteCommand('restart '..PlayerType.PLAYER_BETHANY)A.RemoveCallback(C,B,Z)end A.AddCallback(C,B,Z)
 --.
