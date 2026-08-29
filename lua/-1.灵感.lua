d67bf524228fd5d019f8d8e1d2aaf307023bbc3c
Keye3Tuido <soarcx@foxmail.com>
2026-08-30 02:54:19 +0800

    typo fix

--- lua/-1.灵感.lua
+++ lua/-1.灵感.lua
@@ -37,7 +37,7 @@ l local A,B,C,E,F,G=Isaac.AddCallback,ModCallbacks,PickupVariant.PICKUP_COIN,Get
 --12. 角色每次发射眼泪时，原地生成一个可拾取的炸弹。
 l Isaac.AddCallback({},ModCallbacks.MC_POST_FIRE_TEAR,function(_,t)Isaac.Spawn(EntityType.ENTITY_PICKUP,PickupVariant.PICKUP_THROWABLEBOMB,0,t.Position,Vector.Zero,nil)end)
 
---13. 游戏随机卡顿、删除角色眼泪、删除凋落物。
+--13. 游戏随机卡顿、删除角色眼泪、删除掉落物。
 l local c,d,A,B,Z=Random,pairs,Isaac,EntityType Z=A.FindByType A.AddCallback({},ModCallbacks.MC_POST_UPDATE,function(a,b)a=c()%1e3 if a<8 then for _=1,1e5 do A.GetRoomEntities()end b={}for _,v in d{'HEART','COIN','KEY','BOMB','POOP','GRAB_BAG','PILL','LIL_BATTERY','TAROTCARD','TRINKET'}do a=Z(B.ENTITY_PICKUP,PickupVariant['PICKUP_'..v])table.move(a,1,#a,#b+1,b)end elseif a<24 then b=Z(B.ENTITY_TEAR)end for _,v in d(b or{})do if c()%100<20 then v:Remove()end end end)
 
 --14. 敌人的碰撞箱大小和贴图大小随血量变化。
