e94110e9cb1031a260d97d069dc1a23eda70d955
Keye3Tuido <soarcx@foxmail.com>
2026-09-24 00:44:03 +0800

    清理冗余代码

--- lua/challenges/40.绝对防御.lua
+++ lua/challenges/40.绝对防御.lua
@@ -10,13 +10,6 @@
 ]]
 
 --[[
-模板: force-give-items
-参数:
-  P1: "'c546'"
-  P2: "道具546(爸爸的戒指)"
-]]
-
---[[
 模板: divine-intervention
 ]]
 
@@ -31,16 +24,6 @@
 说明: "投射物自带激光射击"
 ]]
 l Isaac.AddCallback({},ModCallbacks.MC_POST_PROJECTILE_INIT,function(_,p)p:AddProjectileFlags(ProjectileFlags.LASER_SHOT)end)
-
---[[
-说明: "玩家体型不大于2"
-]]
-l Isaac.AddCallback({},ModCallbacks.MC_POST_PLAYER_UPDATE,function(_,p)p.Size=math.min(2,p.Size)end)
-
---[[
-说明: "“爸爸的戒指”光环变大50%"
-]]
-l Isaac.AddCallback({},ModCallbacks.MC_POST_EFFECT_INIT,function(_,e)if e.SubType<1 then e.SpriteScale=3/2*Vector.One e.PositionOffset=Vector(0,26)end end,EffectVariant.HALO)
 
 --===--
 --[[
