a9f4dea80b2b00d454f53cb68efacdb5107b3d27
Keye3Tuido <soarcx@foxmail.com>
2026-09-27 00:01:00 +0800

    41黏币变镍币

--- lua/challenges/41.生化地下室.lua
+++ lua/challenges/41.生化地下室.lua
@@ -49,6 +49,10 @@ l Isaac.AddCallback({},ModCallbacks.MC_POST_UPDATE,function(l,s,r,x,f,a)if Game(
 l local b,a,S=Vector,Isaac,Sprite()S:Load('gfx/ui/loading.anm2',true)S:Play('1',true)S.Scale=b(9,9)S.Color=Color(0,0,0,.2,0,.3)a.AddCallback({},ModCallbacks.MC_POST_RENDER,function()if not IsGridSafe(Game():GetLevel():GetCurrentRoomIndex())then S:RenderLayer(0,b.Zero)end end)
 
 --[[
+模板: sticky-to-nickel
+]]
+
+--[[
 模板: attract-coins
 ]]
 
